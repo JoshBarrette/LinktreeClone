3f64ba7911584f9a0fda6419ee34bef6d2cd5bfd
joshbarrette <joshpbarrette@gmail.com>
2024-01-01 05:40:15 -0600

    Added UserDropDown to NavBar and middleware

---
+++ src/app/MyTree/page.tsx
@@ -0,0 +1,7 @@
+export default function MyTree() {
+    return (
+        <div>
+            <p>MyTree</p>
+        </div>
+    );
+}
--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -5,7 +5,6 @@ import Link from "next/link";
 
 export default function NavBar() {
     const session = useSession();
-    console.log("client session", session.data?.user);
 
     return (
         <div className="relative mx-24 mt-12 flex h-24 rounded-full bg-black text-2xl text-white">
@@ -17,27 +16,55 @@ export default function NavBar() {
 
             <div className="my-auto ml-auto">
                 {session.status === "authenticated" ? (
-                    <div className="mr-3.5 flex">
-                        <div className="my-auto mr-4 scale-150"></div>
-
-                        <button
-                            className="rounded-full bg-white p-5 text-black"
-                            onClick={() => signOut()}
-                        >
-                            Sign Out
-                        </button>
-                    </div>
+                    <UserDropDown user={session.data.user} />
                 ) : (
-                    <a href={process.env.CLERK_SIGN_IN_URL}>
-                        <button
-                            className="mr-3.5 rounded-full bg-white p-5 text-black"
-                            onClick={() => signIn("github")}
-                        >
-                            Sign In
-                        </button>
-                    </a>
+                    <SignInButton />
                 )}
             </div>
         </div>
+    );
+}
+
+function UserDropDown(props: { user: any }) {
+    return (
+        <div className="group mr-4 text-base">
+            <img width={65} src={props.user?.image} className="rounded-full" />
+            <div className="absolute right-0 mr-4 scale-0 group-hover:scale-100">
+                <div className="mt-2 w-40 space-y-1 rounded-lg bg-neutral-700 p-2">
+                    <DropDownItemLink text={"Your Tree"} href="/MyTree" />
+                    <DropDownItem text={"Sign Out"} onClick={() => signOut()} />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function DropDownItem(props: { text: string; onClick?: () => any }) {
+    return (
+        <button
+            className="w-full rounded-md px-4 py-2 text-left transition-all hover:bg-neutral-800"
+            onClick={props.onClick}
+        >
+            {props.text}
+        </button>
+    );
+}
+
+function DropDownItemLink(props: { text: string; href: string }) {
+    return (
+        <Link href={props.href}>
+            <DropDownItem text={props.text} />
+        </Link>
+    );
+}
+
+function SignInButton() {
+    return (
+        <button
+            className="mr-3.5 rounded-full bg-white p-5 text-black"
+            onClick={() => signIn("github")}
+        >
+            Sign In
+        </button>
     );
 }
